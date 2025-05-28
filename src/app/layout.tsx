f1698ea183d8b7e3f4d3a3b3993d84905a71a19e
maamokun/MikanDev <sachadeco214@gmail.com>
2025-05-28 14:39:42 +0900

    fix: og in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,24 @@
 import { Inter, Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
+import type {Metadata} from "next";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const noto_sans_jp = Noto_Sans_JP({
 	subsets: ["latin"],
 	variable: "--font-noto-sans-jp",
 });
+
+export const metadata: Metadata = {
+	title: "Neody UI",
+	description: "Simple and stylish components for your apps, by Neodyland",
+	openGraph: {
+		images: [
+			{
+				url: "https://ui.neody.land/img/NeodyUIOG.png"
+			}
+		]
+	}
+};
 
 export default function RootLayout({
 	children,
--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,18 +1,5 @@
 "use client";
 import { ToastProvider } from "@/components/toast";
-import type {Metadata} from "next";
-
-export const metadata: Metadata = {
-	title: "Neody UI",
-	description: "Simple and stylish components for your apps, by Neodyland",
-	openGraph: {
-		images: [
-			{
-				url: "https://ui.neody.land/img/NeodyUIOG.png"
-			}
-		]
-	}
-};
 
 export default function RootLayout({
 	children,

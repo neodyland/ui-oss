636ba31314f9aefe11c686f54feab204c370750b
maamokun/MikanDev <sachadeco214@gmail.com>
2025-05-28 14:33:55 +0900

    feat: Opengraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { Inter, Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 
@@ -7,11 +6,6 @@ const noto_sans_jp = Noto_Sans_JP({
 	subsets: ["latin"],
 	variable: "--font-noto-sans-jp",
 });
-
-export const metadata: Metadata = {
-	title: "Neody UI",
-	description: "Simple and stylish components for your apps, by Neodyland",
-};
 
 export default function RootLayout({
 	children,
--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,5 +1,18 @@
 "use client";
 import { ToastProvider } from "@/components/toast";
+import type {Metadata} from "next";
+
+export const metadata: Metadata = {
+	title: "Neody UI",
+	description: "Simple and stylish components for your apps, by Neodyland",
+	openGraph: {
+		images: [
+			{
+				url: "https://ui.neody.land/img/NeodyUIOG.png"
+			}
+		]
+	}
+};
 
 export default function RootLayout({
 	children,
